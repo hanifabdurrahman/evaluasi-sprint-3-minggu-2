54e4b21a251102e3b037ee0ecb04bdb4b0eef6da
hanif <hanif@pop-os.localdomain>
2022-09-10 10:47:20 +0700

    evaluasi sprint 3 minggu 2

---
+++ main.js
@@ -0,0 +1,33 @@
+//////no 1
+// const arr = [1, 2, 3, 4, 5]
+// const [a, b, c, d, e] = arr
+// [a, b, c, d, e] = [e, d, c, b, a]
+
+
+/////// no 2
+// function removeArr() {
+//     return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// }
+// const [a,b,c,...value]=removeArr()
+// console.log(value);
+
+
+
+////// no 3
+// let laptop = true
+// const data = new Promise((resolve, reject) => {
+//     if (laptop) {
+//         resolve(
+//             {
+//                 merek: 'nokia',
+//                 kelebihan: 'tahan banting',
+//                 harga: 'Rp 10.000.000'
+
+//             }
+//         )
+//     } else {
+//         reject('Error')
+
+//     }
+// })
+// data.then(res => console.log(res)).catch(err => console.log(err))
